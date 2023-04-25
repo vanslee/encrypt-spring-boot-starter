63bffb800e5e0649f1af29ff68c660df70278f7d
lidengxiang <495234450@qq.com>
2023-04-25 10:00:51 +0800

    更新

--- src/main/java/com/ldx/encrypt/EncryptAutoConfiguration.java
+++ src/main/java/com/ldx/encrypt/EncryptAutoConfiguration.java
@@ -1,4 +1,4 @@
-package com.ldx.encrypt.autoconfig;
+package com.ldx.encrypt;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
